4c01a1f47ae74bb0a31e4ccab11698fe488908b2
swiftttyyy <151486436+swiftttyyy@users.noreply.github.com>
2023-11-27 18:04:13 +0100

    Delete public directory

--- public/js/track.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() 
-{
-    $("body").on('change keyup input click', 'input[name="track"]', function(event)
-    {  
-        let link = $("span.tracker").data("link");
-        $("span.tracker").text(link + $(this).val() + "/");
-    });
-});
